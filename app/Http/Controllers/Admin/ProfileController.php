6116b256d69ec86ef59dea01f913ddcb4546ec95
wmuranak <w.m.2000.wm@gmail.com>
2021-02-08 02:29:44 +0000

    Text No.14 Task

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -13,8 +14,22 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        //validationを行う
+        $this->validate($request,Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+
+        //フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+
+        //DBに保存
+        $profile->timestamps = false; 
+        $profile->fill($form);
+        $profile->save(); 
+ 
         return redirect('admin/profile/create');
     }
 
